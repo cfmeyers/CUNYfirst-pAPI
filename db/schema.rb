f2684ea7313c31396ce59812df1d8bbfb70801c3
Collin Meyers <cfmeyers@gmail.com>
2014-09-24 02:37:40 -0400

    migrate db after addition of Course resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140924053135) do
+ActiveRecord::Schema.define(version: 20140924063318) do
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.string   "cfid"
+    t.integer  "department_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "departments", force: true do |t|
     t.string   "name"
