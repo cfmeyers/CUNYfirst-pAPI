54f752c56e61afeaebda2aac84cd1c3878e0f38b
Collin Meyers <cfmeyers@gmail.com>
2014-09-28 11:21:17 -0400

    create Locations resource

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,50 @@
+class LocationsController < ApplicationController
+  # GET /locations
+  # GET /locations.json
+  def index
+    @locations = Location.all
+
+    render json: @locations
+  end
+
+  # GET /locations/1
+  # GET /locations/1.json
+  def show
+    @location = Location.find(params[:id])
+
+    render json: @location
+  end
+
+  # POST /locations
+  # POST /locations.json
+  # def create
+  #   @location = Location.new(params[:location])
+
+  #   if @location.save
+  #     render json: @location, status: :created, location: @location
+  #   else
+  #     render json: @location.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # # PATCH/PUT /locations/1
+  # # PATCH/PUT /locations/1.json
+  # def update
+  #   @location = Location.find(params[:id])
+
+  #   if @location.update(params[:location])
+  #     head :no_content
+  #   else
+  #     render json: @location.errors, status: :unprocessable_entity
+  #   end
+  # end
+
+  # # DELETE /locations/1
+  # # DELETE /locations/1.json
+  # def destroy
+  #   @location = Location.find(params[:id])
+  #   @location.destroy
+
+  #   head :no_content
+  # end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140928150216) do
+ActiveRecord::Schema.define(version: 20140928151839) do
 
   create_table "courses", force: true do |t|
     t.string   "name"
@@ -40,6 +40,12 @@ ActiveRecord::Schema.define(version: 20140928150216) do
     t.string   "last_name"
     t.string   "first_name"
     t.string   "cfid"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "locations", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
